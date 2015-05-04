5e03ae8718bf330df504ce11227cd11041ec2fc9
Joshua <joshua7v@hotmail.com>
2015-05-04 15:19:45 +0800

    add flask-login

--- project/home/views.py
+++ project/home/views.py
@@ -1,7 +1,8 @@
-from project import app, db
+from project import db
 from project.models import BlogPost
-from flask import flash, redirect, session, url_for, render_template, Blueprint
-from functools import wraps
+from flask import  render_template, Blueprint
+from flask.ext.login import login_required
+# from functools import wraps
 
 
 home_blueprint = Blueprint(
@@ -10,15 +11,15 @@ home_blueprint = Blueprint(
 )
 
 
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('users.login'))
-    return wrap
+# def login_required(test):
+#     @wraps(test)
+#     def wrap(*args, **kwargs):
+#         if 'logged_in' in session:
+#             return test(*args, **kwargs)
+#         else:
+#             flash('You need to login first.')
+#             return redirect(url_for('users.login'))
+#     return wrap
 
 
 @home_blueprint.route('/')
--- project/users/views.py
+++ project/users/views.py
@@ -1,5 +1,8 @@
-from flask import flash, redirect, render_template, request, session, url_for, Blueprint
-from functools import wraps
+from flask import flash, redirect, render_template, request, url_for, Blueprint
+# from functools import wraps
+from flask.ext.login import login_user, login_required, logout_user
+from form import LoginForm
+from project.models import bcrypt, User
 
 
 # config
@@ -9,35 +12,39 @@ users_blueprint = Blueprint(
 )
 
 
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('users.login'))
-    return wrap
+# def login_required(test):
+#     @wraps(test)
+#     def wrap(*args, **kwargs):
+#         if 'logged_in' in session:
+#             return test(*args, **kwargs)
+#         else:
+#             flash('You need to login first.')
+#             return redirect(url_for('users.login'))
+#     return wrap
 
 
 # route
 @users_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
+    form = LoginForm(request.form)
     if request.method == 'POST':
-        if (request.form['username'] != 'admin') \
-                or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in.')
-            return redirect(url_for('home.home'))
-    return render_template('login.html', error=error)
+        if form.validate_on_submit():
+            user = User.query.filter_by(name=request.form['username']).first()
+            if user is not None and bcrypt.check_password_hash(user.passwd, request.form['password']):
+                # session['logged_in'] = True
+                login_user(user)
+                flash('You were logged in.')
+                return redirect(url_for('home.home'))
+            else:
+                error = 'Invalid Credentials. Please try again.'
+    return render_template('login.html', form=form, error=error)
 
 
 @users_blueprint.route('/logout')
 @login_required
 def logout():
-    session.pop('logged_in', None)
+    # session.pop('logged_in', None)
+    logout_user()
     flash('You were logged out.')
     return redirect(url_for('home.welcome'))
